9168bfdff7a5f290e85431df6e46361565f2f2cc
Samuel Odongo <samieodd@gmail.com>
2023-01-02 03:18:23 +0300

    created unit test for page title in scraper

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,6 +1,8 @@
 from unittest import IsolatedAsyncioTestCase
 from pyppeteer import launch
 
+from c_scraper import coriolis_scrape
+
 
 class TestCoriolisScraper(IsolatedAsyncioTestCase):
     async def launch_browser(self) -> None:
@@ -17,3 +19,9 @@ class TestCoriolisScraper(IsolatedAsyncioTestCase):
         await self.page.goto(url='https://s.orbis.zone/lgzu')
         self.pageTitle: str = await self.page.title()
         self.assertIn('EasyChief', self.pageTitle)
+
+    def test_scraper(self) -> None:
+        self.coriolisScraper = coriolis_scrape.CoriolisScraper(
+            'https://s.orbis.zone/lgzu')
+
+        self.assertIn('EasyChief', self.coriolisScraper.pageTitle)
