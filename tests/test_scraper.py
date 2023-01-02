b98c2a0b51acd2f4720f928139ca60f3a94b929a
Samuel Odongo <samieodd@gmail.com>
2023-01-02 04:51:04 +0300

    Split tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -12,7 +12,22 @@ class TestCoriolisScraper(IsolatedAsyncioTestCase):
     async def close_browser(self) -> None:
         await self.browser.close()
 
-    async def test_browser_can_click_button(self) -> None:
+    async def test_coriolisScraper_gets_link(self) -> None:
+        self.coriolisScraper = coriolis_scrape.CoriolisScraper(
+            'https://s.orbis.zone/lgzu')
+
+        self.assertEqual('https://s.orbis.zone/lgzu',
+                         self.coriolisScraper.link)
+
+    async def test_browser_opens_page(self) -> None:
+        self.coriolisScraper = coriolis_scrape.CoriolisScraper(
+            'https://s.orbis.zone/lgzu')
+
+        # call method that gets the web page
+        await self.coriolisScraper.openPage()
+        self.assertIn('EasyChief', self.coriolisScraper.pageTitle)
+
+    async def test_browser_can_identify_button(self) -> None:
         await self.launch_browser()
         # Open link of a coriolis ship build and check the correct page
         # is opened.
@@ -20,8 +35,21 @@ class TestCoriolisScraper(IsolatedAsyncioTestCase):
         self.pageTitle: str = await self.page.title()
         self.assertIn('EasyChief', self.pageTitle)
 
-    def test_scraper(self) -> None:
+    async def test_scraper(self) -> None:
         self.coriolisScraper = coriolis_scrape.CoriolisScraper(
             'https://s.orbis.zone/lgzu')
 
-        self.assertIn('EasyChief', self.coriolisScraper.pageTitle)
+        # call the method that identifies the button that generates
+        # materials in json
+        await self.coriolisScraper.getButton()
+        self.assertIsNotNone(self.coriolisScraper.materialsButton)
+
+    async def test_scraper_can_click_button(self) -> None:
+        self.coriolisScraper = coriolis_scrape.CoriolisScraper(
+            'https://s.orbis.zone/lgzu')
+
+        # Identify button
+        await self.coriolisScraper.getButton()
+
+        # Click the button
+        self.coriolisScraper.clickButton()
