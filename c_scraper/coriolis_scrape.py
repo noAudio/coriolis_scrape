27b16237c13758d7cf3485b590cb6b53fae06413
Samuel Odongo <samieodd@gmail.com>
2023-01-05 00:36:02 +0300

    Created fun to extract and convert special mats

--- c_scraper/coriolis_scrape.py
+++ c_scraper/coriolis_scrape.py
@@ -1,6 +1,8 @@
-from typing import Dict
+from typing import Any, Dict, List
 from pyppeteer import launch, element_handle
 from time import sleep
+
+import json
 
 
 class CoriolisScraper:
@@ -11,6 +13,8 @@ class CoriolisScraper:
     errorMsg: str
     materialsAsText: str
     materials: Dict[str, str]
+    experimentalsMaterials: List[str] = []
+    rawExperimentalsMaterials: Any
 
     def __init__(self, link: str) -> None:
         self.link = link
@@ -70,3 +74,51 @@ class CoriolisScraper:
             print(self.errorMsg)
         self.materialsAsTextJsonValue = await self.materialsHTMLElement.getProperty('textContent')
         self.materialsAsText = await self.materialsAsTextJsonValue.jsonValue()
+
+    def getSpecials(self, componentsSection: Dict[Any, Any]):
+        # Iterate over keys and values in the first nested dictionary
+        for key, value in componentsSection.items():
+            if isinstance(value, dict):
+                # Check for a nested dictionary and then check if its
+                # the dictionary that holds the entries we need, then add
+                # them to a list.
+                if key == 'special':
+                    for k, v in value['components'].items():
+                        self.experimentalsMaterials.append(f'{k}: {v}')
+                # Throw the dictionary back into this function to check the
+                # other nested dictionaries for the required data
+                self.getSpecials(value)
+            else:
+                # Some of the entries will be lists. We need to check for
+                # that then iterate over the nested dictionaries in the list
+                # to get the data
+                if isinstance(value, list):
+                    for item in value:
+                        if isinstance(item, dict):
+                            self.getSpecials(item)
+
+    async def getRawExperimentalsMaterials(self) -> None:
+        ''' Open page and then click the 'Export' button to get the json for the full build details.
+        '''
+        await self.openPage()
+
+        # Check that the button that generates ship build json is
+        # rendered.
+        try:
+            self.exportButton = await self.page.querySelector('#build > button:nth-child(7)')
+        except:
+            self.errorMsg = 'Unable to locate export button'
+
+        await self.exportButton.click()
+
+        # Check for the Export json modal
+        self.exportJsonModal = await self.page.querySelector('#coriolis > div > div.modal-bg > div > div:nth-child(3) > textarea')
+
+        self.rawData = await self.exportJsonModal.getProperty(
+            'textContent')
+        self.jsonOutput = await self.rawData.jsonValue()
+
+        self.rawExperimentalsMaterials = json.loads(self.jsonOutput)
+
+        # Evaluate json and extract materials.
+        self.getSpecials(self.rawExperimentalsMaterials['components'])
