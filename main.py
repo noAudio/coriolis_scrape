accace5bfdbf661fcffd48c8db3d6fb77cfa49d5
Samuel Odongo <samieodd@gmail.com>
2023-01-06 09:55:35 +0300

    Check output of list builder

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import asyncio
-from c_scraper.coriolis_scrape import CoriolisScraper
+from c_scraper.list_builder import ListBuilder
 
 
 async def scrapeItBoi():
-    scraper: CoriolisScraper = CoriolisScraper(
-        link='https://s.orbis.zone/lgzu')
+    listB: ListBuilder = ListBuilder(link='https://s.orbis.zone/lgzu')
 
-    await scraper.getMaterials()
-    await scraper.getRawExperimentalsMaterials()
-    print(scraper.experimentalsMaterials)
-    print(scraper.materialsAsText)
-    print(scraper.buildName)
-    print(scraper.shipName)
+    await listB.generateData()
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(scrapeItBoi())
