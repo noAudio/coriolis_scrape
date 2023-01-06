2e8dc067904da3f468654296521ca4502a7c057a
Samuel Odongo <samieodd@gmail.com>
2023-01-06 12:38:38 +0300

    Create method to clean and convert data

--- c_scraper/list_builder.py
+++ c_scraper/list_builder.py
@@ -6,8 +6,9 @@ from c_scraper.coriolis_scrape import CoriolisScraper
 
 class ListBuilder:
     link: str
-    materialsData: List[Material]
-    generalMaterials: str
+    materialsData: List[Material] = []
+    generalMaterialsRaw: str
+    generalMaterials: List[str]
     specialMaterials: List[str]
     shipName: str
     buildName: str
@@ -24,7 +25,57 @@ class ListBuilder:
         await self.scraper.getMaterials()
         await self.scraper.getRawExperimentalsMaterials()
 
-        self.generalMaterials = self.scraper.materialsAsText
+        self.generalMaterialsRaw = self.scraper.materialsAsText
         self.specialMaterials = self.scraper.experimentalsMaterials
         self.buildName = self.scraper.buildName
         self.shipName = self.scraper.shipName
+
+    def cleanData(self) -> None:
+        '''Convert the general materials from its original string format
+        into a list of strings.
+        '''
+        # Turn general materials into a list of strings by splitting using
+        # line break as the delimitter.
+        self.splitData: List[str] = self.generalMaterialsRaw.split('\n')
+
+        # Remove any empty strings
+        for item in self.splitData:
+            if item == '':
+                self.splitData.remove(item)
+        self.generalMaterials = self.splitData
+
+    def convertData(self, data: List[str]) -> None:
+        # Convert to Material object and append to list
+        for item in data:
+            item = item.split(': ')
+            self.newMaterial: Material = Material(
+                name=item[0], amount=int(item[1]))
+            print(self.newMaterial)
+            self.currentAmount: int
+            self.duplicateIndex: int = -1
+            # Check if the material is already added and if present then
+            # increase its amount. Otherwise append the new material.
+            if self.materialsData:
+                for material in self.materialsData:
+                    if self.newMaterial.name == material.name:
+                        # Note the amount of the duplicate material and
+                        # its index.
+                        self.currentAmount = material.amount
+                        self.duplicateIndex = self.materialsData.index(
+                            material)
+                if (self.duplicateIndex != -1):
+                    # If there is an index for the duplicate material,
+                    # remove it from the list.
+                    self.materialsData.pop(self.duplicateIndex)
+                    self.newMaterial.amount += self.currentAmount
+                self.materialsData.append(self.newMaterial)
+            else:
+                self.materialsData.append(self.newMaterial)
+
+    def convertAllDataToMaterialObjects(self) -> None:
+        '''Converts the general materials and special materials into
+        `Material` objects.
+        '''
+        self.cleanData()
+        self.convertData(data=self.generalMaterials)
+        self.convertData(data=self.specialMaterials)
