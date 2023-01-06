5645f51ce361b799076544eed83cb9a81ed02317
Samuel Odongo <samieodd@gmail.com>
2023-01-06 09:57:59 +0300

    Created class, added method to generate data

---
+++ c_scraper/list_builder.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from c_scraper.materials_model import Material
+from c_scraper.coriolis_scrape import CoriolisScraper
+
+
+class ListBuilder:
+    link: str
+    materialsData: List[Material]
+    generalMaterials: str
+    specialMaterials: List[str]
+    shipName: str
+    buildName: str
+    completeMaterialsData: str  # Hold list of materials as json
+
+    def __init__(self, link: str) -> None:
+        self.link = link
+
+    async def generateData(self) -> None:
+        self.scraper = CoriolisScraper(link=self.link)
+
+        await self.scraper.getMaterials()
+        await self.scraper.getRawExperimentalsMaterials()
+
+        self.generalMaterials = self.scraper.materialsAsText
+        self.specialMaterials = self.scraper.experimentalsMaterials
+
+        self.buildName = self.scraper.buildName
+        self.shipName = self.scraper.shipName
