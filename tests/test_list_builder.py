abe63da3194861432f1bb16cdaa87ecc67d97f4d
Samuel Odongo <samieodd@gmail.com>
2023-01-06 09:09:29 +0300

    Create test to check ListBuilder generates data

---
+++ tests/test_list_builder.py
@@ -0,0 +1,14 @@
+from unittest import IsolatedAsyncioTestCase
+
+from c_scraper.list_builder import ListBuilder
+
+
+class TestListBuilder(IsolatedAsyncioTestCase):
+    testLink: str = 'https://s.orbis.zone/lgzu'
+
+    async def test_list_builder_can_get_materials(self) -> None:
+        self.listBuilder: ListBuilder = ListBuilder(link=self.testLink)
+
+        await self.listBuilder.generateData()
+
+        self.assertIsNotNone(self.listBuilder.materialsData)
