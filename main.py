4a7a39befd4d7d8c8d72e8301a32d47509e0c3ba
Samuel Odongo <samieodd@gmail.com>
2023-01-07 14:13:58 +0300

    Check output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from c_scraper.coriolis_scrape import CoriolisScraper
 from c_scraper.list_builder import ListBuilder
 
 
@@ -6,6 +7,15 @@ async def scrapeItBoi():
     listB: ListBuilder = ListBuilder(link='https://s.orbis.zone/lgzu')
 
     await listB.generateData()
+    listB.convertAllDataToMaterialObjects()
+    print(listB.toJson())
+
+    # scraper: CoriolisScraper = CoriolisScraper(
+    #     link='https://s.orbis.zone/lgzu')
+
+    # await scraper.getMaterials()
+    # print(scraper.materialsAsTextJsonValue)
+    # print(scraper.materialsAsText)
 
 
 loop = asyncio.get_event_loop()
